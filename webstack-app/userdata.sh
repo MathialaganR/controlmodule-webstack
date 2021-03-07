d160d75a0120553430bf0e7b00ed18088e0dcdeb
Mathi R <mathialaganr@hcl.com>
2021-03-07 22:45:51 +0530

    Mathi | userdata updated

--- webstack-app/userdata.sh
+++ webstack-app/userdata.sh
@@ -7,5 +7,11 @@ hostnamectl set-hostname ${appliedhostname}
 #
 # Patch
 #
-yum update -y
-
+sudo yum update -y
+sudo yum install epel-release
+sudo yum install git -y
+sudo yum install ansible -y
+cd /tmp
+sudo git clone https://github.com/MathialaganR/ansible-tomcat-trmb.git
+cd /tmp/ansible-tomcat-trmb/tomcatapp/ansible.d/roles
+ansible-playbook tomcatplaybook.yml
