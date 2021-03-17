53132c4c58e8cf771450326bc724168b37e9eef5
Mathi R <mathialaganr@hcl.com>
2021-03-17 12:52:37 +0530

    Mathi | code fmt done

--- lambda-inventory/main.tf
+++ lambda-inventory/main.tf
@@ -12,10 +12,8 @@ module "lambda_inventory" {
 
   #source = "../../terraform-lambda-trmb/"
 
-  function_name = "ec2inventory_lambda_tf"
-  handler_name  = "lambda_function.lambda_handler"
-  subnet_ids    = ["subnet-01e692f7ef810aa5d","subnet-0836aca4f46d9cbd0"]
+  function_name     = "ec2inventory_lambda_tf"
+  handler_name      = "lambda_function.lambda_handler"
+  subnet_ids        = ["subnet-01e692f7ef810aa5d", "subnet-0836aca4f46d9cbd0"]
   security_group_id = ["sg-0c72f378e687cd39d"]
-
 }
-
